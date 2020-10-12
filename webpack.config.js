d9aeec8fb539192a7536836aad5d693f02f3784c
Christoph Rüßler <christoph.ruessler@mailbox.org>
2020-10-12 16:23:44 +0200

    Add webpack for production build
    Add `.github/workflows/ci.yml`

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/Index.bs.js",
+  mode: "production",
+  output: {
+    path: path.join(__dirname, "bundleOutput"),
+    filename: "index.js",
+  },
+};
+
